4700e80ef21bd0ca85304cd5fbc34d7ed7f9b5f1
Bti <Bti@DESKTOP-OCHCPD4>
2024-11-18 17:45:29 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ToTransfer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rchSQL = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rchTransfer = new System.Windows.Forms.RichTextBox();
+            this.btnDonustur = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // rchSQL
+            // 
+            this.rchSQL.Location = new System.Drawing.Point(14, 61);
+            this.rchSQL.Name = "rchSQL";
+            this.rchSQL.Size = new System.Drawing.Size(728, 120);
+            this.rchSQL.TabIndex = 0;
+            this.rchSQL.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SQL Kod Bloğu :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Yeni Alan :";
+            // 
+            // rchTransfer
+            // 
+            this.rchTransfer.Location = new System.Drawing.Point(14, 336);
+            this.rchTransfer.Name = "rchTransfer";
+            this.rchTransfer.Size = new System.Drawing.Size(728, 139);
+            this.rchTransfer.TabIndex = 3;
+            this.rchTransfer.Text = "";
+            // 
+            // btnDonustur
+            // 
+            this.btnDonustur.Location = new System.Drawing.Point(584, 212);
+            this.btnDonustur.Name = "btnDonustur";
+            this.btnDonustur.Size = new System.Drawing.Size(158, 52);
+            this.btnDonustur.TabIndex = 4;
+            this.btnDonustur.Text = "Dönüştür";
+            this.btnDonustur.UseVisualStyleBackColor = true;
+            this.btnDonustur.Click += new System.EventHandler(this.btnDonustur_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 20;
+            this.listBox.Location = new System.Drawing.Point(809, 61);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(811, 404);
+            this.listBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1652, 518);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.btnDonustur);
+            this.Controls.Add(this.rchTransfer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rchSQL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rchSQL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rchTransfer;
+        private System.Windows.Forms.Button btnDonustur;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToTransfer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDonustur_Click(object sender, EventArgs e)
+        {
+            string text = rchSQL.Text;
+            foreach (char harf in text)
+                //{
+                //    switch (switch_on)
+                //    {
+                //        default:
+                //    }
+                if (true)
+                {
+
+                }
+            }
+        }
+    }
+}
